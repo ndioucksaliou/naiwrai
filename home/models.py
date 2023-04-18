ad4bd6395b7c7c2034409888708db520c406d0fa
Saliou Ndiouck <saliou.ndiouck@neonumy.com>
2023-04-18 17:47:46 +0200

    Add digitalisation and mission model

--- home/models.py
+++ home/models.py
@@ -15,3 +15,31 @@ class HomePage(Page):  # pylint: disable=too-many-ancestors
     class Meta:  # pylint: disable=too-few-public-methods
         """The meta class"""
         verbose_name = "Home Page"
+
+
+class DigitalisationPage(Page):  # pylint: disable=too-many-ancestors
+    """The Digitalisation Page model """
+    introduction = models.CharField(
+        max_length=255,
+        blank=False,
+        null=True,
+    )
+
+    desc_of_digital_section = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("introduction"),
+        FieldPanel("desc_of_digital_section"),
+    ]
+
+    class Meta:  # pylint: disable=too-few-public-methods
+        """The meta class"""
+        verbose_name = "Digitalisation Page"
+
+
+class MissionPage(DigitalisationPage):  # pylint: disable=too-many-ancestors
+    """The Digitalisation Page model """
+
+    class Meta:  # pylint: disable=too-few-public-methods
+        """The meta class"""
+        verbose_name = "Mission Page"
