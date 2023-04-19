52e141ff87dd6e32ef11e5e151a8508b2b7be4a7
Saliou Ndiouck <saliou.ndiouck@neonumy.com>
2023-04-19 14:04:54 +0200

    implement contact features

--- home/models.py
+++ home/models.py
@@ -79,6 +79,7 @@ class ContactPage(DigitalisationPage):  # pylint: disable=too-many-ancestors
         FieldPanel("email"),
         FieldPanel("object"),
         FieldPanel("message"),
+        FieldPanel("conatct_image"),
     ]
 
     class Meta:  # pylint: disable=too-few-public-methods
