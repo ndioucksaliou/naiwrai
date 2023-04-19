d457d2948788f045eaf949a69ef997efd00498ac
Saliou Ndiouck <saliou.ndiouck@neonumy.com>
2023-04-19 11:12:34 +0200

    add contact model

--- home/models.py
+++ home/models.py
@@ -43,3 +43,44 @@ class MissionPage(DigitalisationPage):  # pylint: disable=too-many-ancestors
     class Meta:  # pylint: disable=too-few-public-methods
         """The meta class"""
         verbose_name = "Mission Page"
+
+
+class ContactPage(DigitalisationPage):  # pylint: disable=too-many-ancestors
+    """The Digitalisation Page model """
+    name = models.CharField(
+        max_length=255,
+        blank=False,
+        null=True,
+    )
+    email = models.EmailField(
+        max_length=255,
+        blank=False,
+        null=True,
+        default="contact@newrai.fr",
+    )
+    object = models.CharField(
+        max_length=255,
+        blank=False,
+        null=True,
+    )
+    message = RichTextField(blank=False)
+    conatct_image = models.ForeignKey(
+        "wagtailimages.Image",
+        blank=True,
+        null=True,
+        related_name="+",
+        on_delete=models.SET_NULL,
+        help_text='Choisir votre contact image',
+    )
+    content_panels = Page.content_panels + [
+        FieldPanel("introduction"),
+        FieldPanel("desc_of_digital_section"),
+        FieldPanel("name"),
+        FieldPanel("email"),
+        FieldPanel("object"),
+        FieldPanel("message"),
+    ]
+
+    class Meta:  # pylint: disable=too-few-public-methods
+        """The meta class"""
+        verbose_name = "Contact Page"
