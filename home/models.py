440e1ccdafb9f3baad875ec0d32f4682dd1c97dd
Saliou Ndiouck <saliou.ndiouck@neonumy.com>
2023-04-26 12:35:53 +0200

    add new model of HomePage and apply them in template

--- home/models.py
+++ home/models.py
@@ -1,15 +1,188 @@
 from django.db import models
 
-from wagtail.models import Page
+from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel, InlinePanel
+from wagtail.snippets.models import register_snippet
+from modelcluster.fields import ParentalKey
+
+
+@register_snippet
+class Home(models.Model):
+    """ The home model"""
+    title = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+    )
+    description = RichTextField(
+        blank=True,
+        null=True,
+    )
+
+    panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel("title"),
+                FieldPanel("description"),
+            ]
+        )
+    ]
+
+    def __str__(self) -> str:
+        return str(self.title)
+
+
+class HomePageHomePlacement(Orderable, models.Model):
+    """ The home placement """
+    page = ParentalKey(
+        "home.HomePage",
+        on_delete=models.CASCADE,
+        related_name="home_placements"
+    )
+    home = models.ForeignKey(
+        "home.Home",
+        on_delete=models.CASCADE,
+        related_name='+',
+    )
+
+    panels = [
+        FieldPanel('home'),
+    ]
+
+    def __str__(self):
+        return self.page.title + " -> " + self.home.title
+
+    class Meta(Orderable.Meta): # pylint: disable=too-few-public-methods
+        """ The meta model"""
+        verbose_name = "home placement"
+        verbose_name_plural = "home placements"
 
 
 class HomePage(Page):  # pylint: disable=too-many-ancestors
     """The home page model """
+    # ==================== Hero Section Model========================
     home_description = RichTextField(blank=True)
+    home_image_logo = models.ForeignKey(
+        "wagtailimages.Image",
+        blank=True,
+        null=True,
+        related_name="+",
+        on_delete=models.SET_NULL,
+    )
+    # ==================== Offer Section Model ==================
+
+    # ===================== Mission Section Model =========================
+    home_image_mission = models.ForeignKey(
+        "wagtailimages.Image",
+        blank=True,
+        null=True,
+        related_name="+",
+        on_delete=models.SET_NULL,
+    )
+    first_intro_mission = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        help_text="Ajouter la 1ére introduction de mission",
+    )
+    second_intro_mission = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        help_text="Ajouter la 2ème introduction de mission",
+    )
+    first_desc_mission = RichTextField(blank=True)
+    second_desc_mission = RichTextField(blank=True)
+    # ====================== Vision Section Model =============================
+    home_image_vision = models.ForeignKey(
+        "wagtailimages.Image",
+        blank=True,
+        null=True,
+        related_name="+",
+        on_delete=models.SET_NULL,
+    )
+
+    home_title_vision = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        help_text="Ajouter le titre de vision",
+    )
+
+    home_intro_vision = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        help_text="Ajouter l'introduction de vision",
+    )
+
+    home_desc_vision = RichTextField(blank=True)
+
+    # ====================== Project Section Model =============================
+    home_image_project = models.ForeignKey(
+        "wagtailimages.Image",
+        blank=True,
+        null=True,
+        related_name="+",
+        on_delete=models.SET_NULL,
+    )
+
+    home_desc_project = RichTextField(blank=True)
+
+    # ===================== Novelty Section model ==============================
+    home_image_novelty = models.ForeignKey(
+        "wagtailimages.Image",
+        blank=True,
+        null=True,
+        related_name="+",
+        on_delete=models.SET_NULL,
+    )
+
+    home_desc_novelty = RichTextField(blank=True)
+
+    # =================== Commitment Section Model ============================
+    home_image_commitment = models.ForeignKey(
+        "wagtailimages.Image",
+        blank=True,
+        null=True,
+        related_name="+",
+        on_delete=models.SET_NULL,
+    )
+
+    home_desc_commitment = RichTextField(blank=True)
+
     content_panels = Page.content_panels + [
+        # ======= Hero Section panel
         FieldPanel("home_description"),
+        FieldPanel("home_image_logo"),
+
+        # ======= Mission Section Pannel
+        FieldPanel("home_image_mission"),
+        FieldPanel("first_intro_mission"),
+        FieldPanel("first_desc_mission"),
+        FieldPanel("second_intro_mission"),
+        FieldPanel("second_desc_mission"),
+
+        # ========= Vision Section Panel
+        FieldPanel("home_image_vision"),
+        FieldPanel("home_title_vision"),
+        FieldPanel("home_intro_vision"),
+        FieldPanel("home_desc_vision"),
+
+        # ========= Project Section Panel
+        FieldPanel("home_image_project"),
+        FieldPanel("home_desc_project"),
+
+        # ========= Novelty Section Panel
+        FieldPanel("home_image_novelty"),
+        FieldPanel("home_desc_novelty"),
+
+        # ========= Commitment Section Panel
+        FieldPanel("home_image_commitment"),
+        FieldPanel("home_desc_commitment"),
+
+        InlinePanel("home_placements", label="Home"),
     ]
 
     class Meta:  # pylint: disable=too-few-public-methods
@@ -77,6 +250,12 @@ class DigitalisationPage(Page):  # pylint: disable=too-many-ancestors
         null=True,
     )
     app_desc = RichTextField(blank=True)
+    first_intro_mission = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        help_text="1ère introduction de l'offre",
+    )
     digital_revolution_title = models.CharField(
         max_length=255,
         blank=True,
@@ -221,8 +400,14 @@ class MissionPage(DigitalisationPage):  # pylint: disable=too-many-ancestors
         verbose_name = "Mission Page"
 
 
-class ContactPage(DigitalisationPage):  # pylint: disable=too-many-ancestors
+class ContactPage(Page):  # pylint: disable=too-many-ancestors
     """The Digitalisation Page model """
+    introduction = models.CharField(
+        max_length=255,
+        blank=False,
+        null=True,
+    )
+    description = RichTextField(blank=False)
     name = models.CharField(
         max_length=255,
         blank=False,
@@ -250,7 +435,7 @@ class ContactPage(DigitalisationPage):  # pylint: disable=too-many-ancestors
     )
     content_panels = Page.content_panels + [
         FieldPanel("introduction"),
-        FieldPanel("desc_of_digital_section"),
+        FieldPanel("description"),
         FieldPanel("name"),
         FieldPanel("email"),
         FieldPanel("object"),
