47b26cd270847501e43762f262b5f5c30f2d0003
Hélio Correia <helio.as.correia@gmail.com>
2023-06-07 13:06:29 +0000

    Quick fix

--- newrai/app/settings/development.py
+++ newrai/app/settings/development.py
@@ -41,8 +41,8 @@ DATABASES = {
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = "neonumy-uploads-development"
-AWS_S3_ACCESS_KEY_ID = os.environ['AWS_S3_ACCESS_KEY_ID']
-AWS_S3_SECRET_ACCESS_KEY = os.environ['AWS_S3_SECRET_ACCESS_KEY']
+AWS_S3_ACCESS_KEY_ID = get_env('AWS_ACCESS_KEY_ID')
+AWS_S3_SECRET_ACCESS_KEY = get_env('AWS_SECRET_ACCESS_KEY')
 AWS_LOCATION = "newrai"
 AWS_QUERYSTRING_AUTH = False
 AWS_DEFAULT_ACL = "public-read"
